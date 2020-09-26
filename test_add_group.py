2e72b3a4daf3706b30803ffa3890c421529d84b4
OlgaBrozhe <olya_lek@yahoo.com>
2020-09-25 23:52:52 -0700

    refactoring - introduced variables for methods create_new_group,login,open_home_page

--- test_add_group.py
+++ test_add_group.py
@@ -11,15 +11,15 @@ class TestAddGroup(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd)
-        self.create_new_group(wd)
+        self.open_home_page(wd, URL_addressbook="http://localhost/addressbook/")
+        self.login(wd, username="admin", password="secret")
+        self.create_new_group(wd, group_name="TestNewGroup", group_header="TestNewGroup", group_footer="TestNewGroup")
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def create_new_group(self, wd):
+    def create_new_group(self, wd, group_name, group_header, group_footer):
         # open groups page
         wd.find_element_by_link_text("groups").click()
         # open new group creation page
@@ -27,27 +27,27 @@ class TestAddGroup(unittest.TestCase):
         # fill in new group creation form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("TestNewGroup")
+        wd.find_element_by_name("group_name").send_keys(group_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("TestNewGroup")
+        wd.find_element_by_name("group_header").send_keys(group_header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("TestNewGroup")
+        wd.find_element_by_name("group_footer").send_keys(group_footer)
         # Submit the group creation
         wd.find_element_by_name("submit").click()
         # return to groups page
         wd.find_element_by_link_text("group page").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
+    def open_home_page(self, wd, URL_addressbook):
+        wd.get(URL_addressbook)
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
