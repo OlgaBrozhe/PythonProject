b51d08e2b744943c9d1a0334ec60be0ddc0b2654
OlgaBrozhe <olya_lek@yahoo.com>
2020-09-27 23:33:24 -0700

    Introduced methods open_home_page(self, wd, url_addressbook) login(self, wd, username, password) add_new_contact(self, wd, contact_form), added comments.
    Introduced class ContactForm (contact_name, contact_lastname, contact_email, contact_company, contact_mobile)
    
    Test run successfully.

---
+++ contact_form.py
@@ -0,0 +1,8 @@
+class ContactForm:
+
+    def __init__(self, contact_name, contact_lastname, contact_email, contact_company, contact_mobile):
+        self.contact_name = contact_name
+        self.contact_lastname = contact_lastname
+        self.contact_email = contact_email
+        self.contact_company = contact_company
+        self.contact_mobile = contact_mobile
--- test_add_contact.py
+++ test_add_contact.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from contact_form import ContactForm
 import unittest
 
 class TestAddContact(unittest.TestCase):
@@ -11,31 +12,42 @@ class TestAddContact(unittest.TestCase):
 
     def test_add_contact(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_id("LoginForm").submit()
+        self.open_home_page(wd, "http://localhost/addressbook/")
+        self.login(wd, "admin", "secret")
+        self.add_new_contact(wd, ContactForm("TestUserName1", "TestUserLastName1", "TestUserEmail1", "TestUserCompany", "TestUserTelephoneMobile1"))
+
+    def add_new_contact(self, wd, contact_form):
         wd.find_element_by_link_text("add new").click()
+        #Fill in the contact info
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("TestUserName1")
+        wd.find_element_by_name("firstname").send_keys(contact_form.contact_name)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("TestUserLastName1")
+        wd.find_element_by_name("lastname").send_keys(contact_form.contact_lastname)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("TestUserEmail1")
+        wd.find_element_by_name("email").send_keys(contact_form.contact_email)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("TestUserCompany")
+        wd.find_element_by_name("company").send_keys(contact_form.contact_company)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("TestUserTelephoneMobile1")
+        wd.find_element_by_name("mobile").send_keys(contact_form.contact_mobile)
+        #Submit and return to Home page
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         wd.find_element_by_link_text("home page").click()
-    
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_id("LoginForm").submit()
+
+    def open_home_page(self, wd, url_addressbook):
+        wd.get(url_addressbook)
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
@@ -56,10 +68,9 @@ class TestAddContact(unittest.TestCase):
                 alert.dismiss()
             return alert_text
         finally: self.accept_next_alert = True
-    
+
     def tearDown(self):
         self.wd.quit()
-        self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
