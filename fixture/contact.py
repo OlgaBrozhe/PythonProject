f29726c42c6837cfd7a2f80fbd52c145e7c5540b
OlgaBrozhe <olya_lek@yahoo.com>
2020-10-01 19:10:50 -0700

    Refactoring - extracted methods for filling in the contact and groups forms, edited methods comments.

--- fixture/contact.py
+++ fixture/contact.py
@@ -5,8 +5,15 @@ class ContactHelper:
 
     def create(self, contact_form):
         wd = self.app.wd
+        #Create a new contact and return to home page
         wd.find_element_by_link_text("add new").click()
-        #Fill in the contact info
+        self.fill_contact_form(contact_form)
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        wd.find_element_by_link_text("home page").click()
+
+    def fill_contact_form(self, contact_form):
+        wd = self.app.wd
+        #Fill in contact info: first name, last name, email, company, mobile
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact_form.contact_name)
@@ -22,39 +29,19 @@ class ContactHelper:
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
         wd.find_element_by_name("mobile").send_keys(contact_form.contact_mobile)
-        #Submit and return to Home page
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        wd.find_element_by_link_text("home page").click()
 
     def del_first(self):
         wd = self.app.wd
-        # select first
+        #Delete first contact
         wd.find_element_by_xpath("(//input[@name='selected[]'])").click()
-        # Submit the deletion
         wd.find_element_by_xpath("(//input[@value='Delete'])").click()
         wd.switch_to_alert().accept()
 
     def mod_first(self, contact_form):
         wd = self.app.wd
-        # select first and open to edit
+        #Modify first contact and return to home page
         wd.find_element_by_xpath("(//input[@name='selected[]'])").click()
         wd.find_element_by_xpath("(//img[@alt='Edit'])").click()
-        #Modify the contact info
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact_form.contact_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact_form.contact_lastname)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact_form.contact_email)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact_form.contact_company)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact_form.contact_mobile)
-        # Submit the modification and return to Home page
+        self.fill_contact_form(contact_form)
         wd.find_element_by_xpath("(//input[@value='Update'])").click()
         wd.find_element_by_link_text("home page").click()
--- fixture/group.py
+++ fixture/group.py
@@ -5,10 +5,16 @@ class GroupHelper:
 
     def create(self, group_form):
         wd = self.app.wd
+        #Create a new group and return to groups page
         self.open_groups_page()
-        # open new group creation page
         wd.find_element_by_name("new").click()
-        # fill in new group creation form
+        self.fill_group_form(group_form)
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def fill_group_form(self, group_form):
+        wd = self.app.wd
+        #Fill in group info: name, header, footer
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group_form.group_name)
@@ -18,43 +24,31 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group_form.group_footer)
-        # Submit the group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
 
     def del_first(self):
         wd = self.app.wd
+        #Delete first group and return to groups page
         self.open_groups_page()
-        #select first group
         wd.find_element_by_name("selected[]").click()
-        # Submit the group deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
     def mod_first(self, group_form):
         wd = self.app.wd
+        #Modify first group and return to groups page
         self.open_groups_page()
-        #select first group and open to edit
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
-        # edit the group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group_form.group_name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group_form.group_header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group_form.group_footer)
-        # Submit the group update
+        self.fill_group_form(group_form)
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
     def return_to_groups_page(self):
         wd = self.app.wd
+        #Return to groups page after creation, deletion or modification of a group
         wd.find_element_by_link_text("group page").click()
 
     def open_groups_page(self):
         wd = self.app.wd
+        #Open groups page from menu
         wd.find_element_by_link_text("groups").click()
