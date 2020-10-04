e416aaca6c777c6e7b7b8b3e2ad88fbf2bc38754
OlgaBrozhe <olya_lek@yahoo.com>
2020-10-03 22:03:50 -0700

    Added check that the fixture is valid

--- conftest.py
+++ conftest.py
@@ -3,10 +3,25 @@ from fixture.application import Application
 import pytest
 
 
-@pytest.fixture(scope = "session")
+fixture = None
+
+@pytest.fixture
 def app(request):
-    fixture = Application()
-    fixture.session.login("admin", "secret")
+    global fixture
+    #If fixture is valid:
+    if fixture is None:
+        fixture = Application()
+        fixture.session.login("admin", "secret")
+    #If fixture is invalid, e.g. browser failed
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+            fixture.session.login("admin", "secret")
+    return fixture
+
+
+@pytest.fixture(scope = "session", autouse=True)
+def app_stop(request):
     def fin():
         fixture.session.logout()
         fixture.destroy()
