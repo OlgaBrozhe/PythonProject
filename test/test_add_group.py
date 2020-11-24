1910850b35f3d465ed6101f823834bb29f0e8be1
OlgaBrozhe <olya_lek@yahoo.com>
2020-11-24 11:20:10 -0800

    Edited test to delete a group to use BDD inside the test. Ran add group and del group tests, collected results and generated Allure reports.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,5 +1,7 @@
 from model.group_form import GroupForm
 import re
+import pytest
+import allure
 
 
 # Name of the parameter where to put the test data, where to take it from and its text representation
@@ -10,20 +12,22 @@ import re
 
 def test_add_group(app, db, json_data_groups, check_ui):
     group = json_data_groups
-    old_groups_list = db.get_db_groups_list()
-    app.group.create(group)
-    new_groups_list = db.get_db_groups_list()
-    # Append old list with new item, clear and sort lists ascending and check if they are still equal
-    old_groups_list.append(group)
-    # Check match of UI and DB lists
-    if check_ui:
-        sorted_db_new_groups_list = sorted([clear(x) for x in new_groups_list], key=GroupForm.id_or_max)
-        sorted_ui_new_groups_list = sorted([clear(x) for x in app.group.get_groups_list()], key=GroupForm.id_or_max)
-        assert sorted_db_new_groups_list == sorted_ui_new_groups_list
+    with allure.step('Given a group list'):
+        old_groups_list = db.get_db_groups_list()
+    with allure.step('When I add group %s to the list' % group):
+        app.group.create(group)
+    with allure.step('Then the new group list is equal to the old list with the added group'):
+        new_groups_list = db.get_db_groups_list()
+        # Append old list with new item, clear and sort lists ascending and check if they are still equal
+        old_groups_list.append(group)
+        # Check match of UI and DB lists
+        if check_ui:
+            sorted_db_new_groups_list = sorted([clear(x) for x in new_groups_list], key=GroupForm.id_or_max)
+            sorted_ui_new_groups_list = sorted([clear(x) for x in app.group.get_groups_list()], key=GroupForm.id_or_max)
+            assert sorted_db_new_groups_list == sorted_ui_new_groups_list
 
 
 def clear(s):
     group_name_cleared = re.sub("[() -]", "", s.group_name)
     s.group_name = group_name_cleared
     return s
-
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,27 +1,32 @@
 from model.group_form import GroupForm
 import random
 import re
+import pytest
+import allure
 
 
 def test_del_group(app, db, check_ui):
     # Check if any group exist, and if not - create one
-    old_groups_list = db.get_db_groups_list()
-    if not old_groups_list:
-        app.group.create(GroupForm(group_name="AutocreatedGroup"))
+    with allure.step('Given a non-empty group list'):
         old_groups_list = db.get_db_groups_list()
-    group = random.choice(old_groups_list)
-    app.group.del_by_id(group.group_id)
-    new_groups_list = db.get_db_groups_list()
-    # First check - if group was deleted at all
-    assert len(old_groups_list) - 1 == len(new_groups_list)
-    # Second check - if remained groups are equal
-    old_groups_list.remove(group)
-    assert old_groups_list == new_groups_list
-    # Check match of UI and DB lists
-    if check_ui:
-        sorted_db_new_groups_list = sorted([clear(x) for x in new_groups_list], key=GroupForm.id_or_max)
-        sorted_ui_new_groups_list = sorted([clear(x) for x in app.group.get_groups_list()], key=GroupForm.id_or_max)
-        assert sorted_db_new_groups_list == sorted_ui_new_groups_list
+        if not old_groups_list:
+            app.group.create(GroupForm(group_name="AutocreatedGroup"))
+            old_groups_list = db.get_db_groups_list()
+        group = random.choice(old_groups_list)
+    with allure.step('When I delete the group %s from the list' % group):
+        app.group.del_by_id(group.group_id)
+    with allure.step('Then the new group list is equal to the old group list without the deleted group'):
+        new_groups_list = db.get_db_groups_list()
+        # First check - if group was deleted at all
+        assert len(old_groups_list) - 1 == len(new_groups_list)
+        # Second check - if remained groups are equal
+        old_groups_list.remove(group)
+        assert old_groups_list == new_groups_list
+        # Check match of UI and DB lists
+        if check_ui:
+            sorted_db_new_groups_list = sorted([clear(x) for x in new_groups_list], key=GroupForm.id_or_max)
+            sorted_ui_new_groups_list = sorted([clear(x) for x in app.group.get_groups_list()], key=GroupForm.id_or_max)
+            assert sorted_db_new_groups_list == sorted_ui_new_groups_list
 
 def clear(s):
     group_name_cleared = re.sub("[() -]", "", s.group_name)
